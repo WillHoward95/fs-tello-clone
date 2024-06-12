38e789859f3359874cb2592136b0d6db51c05c6e
Will Howard <willhowardfreelance@gmail.com>
2024-06-12 16:33:15 +0100

    project setup

---
+++ packages/client/src/components/Board.tsx
@@ -0,0 +1,11 @@
+import NewColumn from "./forms/NewColumn";
+
+const Board = () => {
+  return (
+    <div>
+      <NewColumn />
+    </div>
+  );
+};
+
+export default Board;
---
+++ packages/client/src/components/forms/NewColumn.tsx
@@ -0,0 +1,8 @@
+export default function NewColumn() {
+  return (
+    <form>
+      <input type="text" placeholder="add a new column" />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
